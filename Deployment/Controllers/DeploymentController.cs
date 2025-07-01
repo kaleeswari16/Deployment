ce806a694966983a377860fed60b7754f095261e
kaleeswari16 <kalees16062002@gmail.com>
2025-07-01 20:43:21 +0530

    First Commit

---
+++ Deployment/Controllers/DeploymentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Deployment.Controllers
+{
+    public class DeploymentController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
